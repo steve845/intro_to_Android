a67c646c4741ba40a882155a189b8f0be257272a
steve <stevestevew7@gmail.com>
2021-06-10 23:38:23 +0300

    update changes

--- app/src/main/java/com/example/intro_to_android1/MainActivity.kt
+++ app/src/main/java/com/example/intro_to_android1/MainActivity.kt
@@ -2,17 +2,23 @@ package com.example.intro_to_android1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-
- lateinit var textView: TextView
+   lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.text_view)
+    }
+    fun pressButtonAction(view: View) {
+        //print"hello"
 
     }
+
+
 
 }
